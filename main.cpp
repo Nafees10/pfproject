27d999f0a018e32259d5a4496fba0c8ebcd4d899
Nafees10 <nafeeshassan16@gmail.com>
2021-12-25 17:26:07 +0500

    added debug

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 #include "usfml.h"
 
 int main(){
-	createWindow(800, 600, (char*)"potato");
-	std::cin.ignore(1000, '\n');
-	destroyWindow();
+	createWindow(800, 600, (char*)"Potato Game");
+	while (windowIsOpen()){
+		Event event;
+		eventWait();
+		std::cout << "event type is: " << event.type << "\n";
+		if (eventGet(event) && event.type == EventType::WindowCloseButtonPress)
+			destroyWindow();
+	}
+	std::cin.ignore (1000, '\n');
 	return 0;
 }
