94a65f8d3999fd36e9d2dfeca68d4ea3f9e335b2
Nafees10 <nafeeshassan16@gmail.com>
2021-12-26 19:58:55 +0500

    renamed functions

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include "usfml.h"
+#include "base.h"
 
 int main(){
-	createWindow(800, 600, (char*)"Potato Game");
+	usfmlInit(800, 600, (char*)"Potato Game");
 	while (windowIsOpen()){
 		Event event;
 		eventWait();
 		std::cout << "event type is: " << event.type << "\n";
 		if (eventGet(event) && event.type == EventType::WindowCloseButtonPress)
-			destroyWindow();
+			usfmlDestroy();
 	}
 	std::cin.ignore (1000, '\n');
 	return 0;
